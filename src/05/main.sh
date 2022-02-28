81313613e78ad6725285df566831aa33cc2f6257
Maricela Tinisha <matinish@si-b3.nsk.21-school.ru>
2022-02-28 23:09:21 +0700

    05 in progress

--- src/05/main.sh
+++ src/05/main.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+source date.sh
+source system.sh
+
+IFS=""
+
+dir=$1
+
+if [ -d $1 ]; then
+    echo $system
+fi
---
+++ src/05/system.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+system=$(echo "Total number of folders (including all nested ones) = $folders"
+echo "TOP 5 folders of maximum size arranged in descending order (path and size):"
+echo $topfol
+echo etc up to 5
+echo Total number of files = $files
+)
